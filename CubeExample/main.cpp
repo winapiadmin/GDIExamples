6c15d5c417166e14769fe03a5bf6e494f2417a93
winapiadmin <138602885+winapiadmin@users.noreply.github.com>
2023-08-05 13:58:22 +0700

    Update main.cpp
    Added functionality for color support of the cube, except colored cubes.

--- CubeExample/main.cpp
+++ CubeExample/main.cpp
@@ -1,7 +1,7 @@
 #include <windows.h>
 #include <wingdi.h>
 #include <winuser.h>
-
+#ifdef NOFACECOLOR
 void DrawCube(HDC hdc, int x, int y)
 {
 	MoveToEx(hdc, x, y, (LPPOINT)NULL);
@@ -17,7 +17,62 @@ void DrawCube(HDC hdc, int x, int y)
 	LineTo(hdc, 30 + x, 130 + y);
 
 }
-
+#endif
+#if _MSC_VER>=1500
+#ifndef NOFACECOLOR
+void DrawCube(HDC hdc, int x, int y,COLORREF color1,COLORREF color2,COLORREF color3,COLORREF color4)
+{
+	
+	SelectObject(hdc, GetStockObject(NULL_BRUSH));
+	HBRUSH brush=CreateSolidBrush(color1);
+	SelectObject(hdc,brush);
+	Rectangle(hdc, 0 + x, 0 + y, 100 + x, 100 + y);
+	DeleteObject(brush);
+	brush=CreateSolidBrush(color2);
+	SelectObject(hdc,brush);
+	Rectangle(hdc, 30 + x, 30 + y, 130 + x, 130 + y);
+	DeleteObject(brush);
+	brush=CreateSolidBrush(color3);
+	SelectObject(hdc,brush);
+	POINT points[5]{{30+x,30+y},{30+x,30+y},{x,y},{100+x,0+y},{130+x,30+y}};
+	Polygon(hdc,points,5);
+	DeleteObject(brush);
+	brush=CreateSolidBrush(color4);
+	SelectObject(hdc,brush);
+	POINT points2[8]{{x,y},{30+x,30+y},{x,y},{0+x,100+y},{0+x,100+y},{30+x,130+y},{30+x,130+y},{30+x,30+y}};
+	Polygon(hdc,points2,8);
+	DeleteObject(brush);
+}
+#endif
+#endif
+#if _MSC_VER<=1200
+#ifndef NOFACECOLOR
+void DrawCube(HDC hdc, int x, int y,COLORREF color1,COLORREF color2,COLORREF color3,COLORREF color4)
+{
+	
+	SelectObject(hdc, GetStockObject(NULL_BRUSH));
+	HBRUSH brush=CreateSolidBrush(color1);
+	SelectObject(hdc,brush);
+	Rectangle(hdc, 0 + x, 0 + y, 100 + x, 100 + y);
+	DeleteObject(brush);
+	brush=CreateSolidBrush(color2);
+	SelectObject(hdc,brush);
+	Rectangle(hdc, 30 + x, 30 + y, 130 + x, 130 + y);
+	DeleteObject(brush);
+	brush=CreateSolidBrush(color3);
+	SelectObject(hdc,brush);
+	POINT points[5]={{30+x,30+y},{30+x,30+y},{x,y},{100+x,0+y},{130+x,30+y}};
+	Polygon(hdc,points,5);
+	DeleteObject(brush);
+	brush=CreateSolidBrush(color4);
+	SelectObject(hdc,brush);
+	POINT points2[8]={{x,y},{30+x,30+y},{x,y},{0+x,100+y},{0+x,100+y},{30+x,130+y},{30+x,130+y},{30+x,30+y}};
+	Polygon(hdc,points2,8);
+	DeleteObject(brush);
+}
+#endif
+#endif
+#ifdef NOFACECOLOR
 void Draw3x3Cube(HDC hdc, int x, int y)
 {
 	DrawCube(hdc, x, y);
@@ -50,4 +105,4 @@ void Draw3x3Cube(HDC hdc, int x, int y)
 	DrawCube(hdc, x + 100 + 30 + 30, y + 200 + 30 + 30);
 	DrawCube(hdc, x + 200 + 30 + 30, y + 200 + 30 + 30);
 }
-
+#endif
