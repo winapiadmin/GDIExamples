cf52d5220305ade5d27c88930a81ffb32b7ff4c3
winapiadmin <138602885+winapiadmin@users.noreply.github.com>
2023-10-01 16:45:19 +0700

    Update StretchBltExample.cpp

--- ShiftingScreen/StretchBltExample.cpp
+++ ShiftingScreen/StretchBltExample.cpp
@@ -1,13 +1,14 @@
 #include <windows.h>
 #include <wingdi.h>
 #include <winuser.h>
-
-void StretchBltShift(HDC hdc){
+#include <math.h>
+void StretchBltCopyOutside(HDC hdc,int optionalvalue){ // actually copying outside HDC
       RECT screen;
       // Because client coordinates are relative to the upper-left corner of a window's client area
       // the coordinates of the upper-left corner are (0,0).
       GetClientRect(WindowFromDC(hdc),&screen);
       int ScaleWidth = screen.right, ScaleHeight = screen.bottom;
-      StretchBlt hdc, -1, -1, ScaleWidth - 1, ScaleHeight - 1, hdc, 0, 0, ScaleWidth - 1, ScaleHeight - 1, SRCERASE;
-      StretchBlt hdc, -1, -1, ScaleWidth - 1, ScaleHeight - 1, hdc, 0, 0, ScaleWidth - 1, ScaleHeight - 1, PATINVERT;
+      int optionalvalue2=abs(optionalvalue);
+      // StretchBlt(hdc, -optionalvalue, -optionalvalue, ScaleWidth - optionalvalue, ScaleHeight - optionalvalue, hdc, optionalvalue, optionalvalue, ScaleWidth - optionalvalue, ScaleHeight - optionalvalue, NOTSRCERASE); // optional 
+      StretchBlt(hdc, -optionalvalue2, -optionalvalue2, ScaleWidth - optionalvalue2, ScaleHeight - optionalvalue2, hdc, optionalvalue2, optionalvalue2, ScaleWidth - optionalvalue2, ScaleHeight - optionalvalue2, PATINVERT);
 }
